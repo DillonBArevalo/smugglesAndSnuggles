DillonBArevalo/smugglesAndSnuggles

commit 036a6a3f960ef5821c343159c29cfc4243c419d0

Fix game generation bug where I was checking the wrong cards for deciding first turn

app/models/game.rb
@@ -95,13 +95,13 @@ class Game < ApplicationRecord
   private
 
   def self.decide_first_move(city, country)
-    if city[-1][:value] == country[1][:value] && city[1][:value] = country[-1][:value]
+    if city[0][:value] == country[-2][:value] && city[-2][:value] = country[0][:value]
       return decide_first_move(city.shuffle!, country.shuffle!)
-    elsif city[-1][:value] == country[1][:value]
-      city[1], city[-1] = city[-1], city[1]
-      country[1], country[-1] = country[-1], country[1]
+    elsif city[0][:value] == country[-2][:value]
+      city[-2], city[0] = city[0], city[-2]
+      country[-2], country[0] = country[0], country[-2]
     end
-    city[-1][:value] > country[1][:value] ? 'country' : 'city'
+    city[0][:value] > country[-2][:value] ? 'country' : 'city'
   end
 
   def self.build_deck(deck_name)

app/javascript/packs/Components/Game.jsx
@@ -81,7 +81,6 @@ class Game extends Component {
 
     ({movesLeft, movedCardValue, winner, activeDeck} = this.checkScore(endRow, movingCard, board, movesLeft, movedCardValue));
     if(this.state.law === 1 && this.isOnLaw(endRow, endCol)){
-      console.log('here')
       activeDeck = this.state.activeDeck;
       movedCardValue.push(movingCard.value);
     }else if(movesLeft === 1){

commit e7e8a68d06a1509a9e58f72b60527954099c85d6

Fix bug introduced in 036a6a3f where I had an assignment in decide_first_move

app/models/game.rb
@@ -95,7 +95,7 @@ class Game < ApplicationRecord
   private
 
   def self.decide_first_move(city, country)
-    if city[0][:value] == country[-2][:value] && city[-2][:value] = country[0][:value]
+    if city[0][:value] == country[-2][:value] && city[-2][:value] == country[0][:value]
       return decide_first_move(city.shuffle!, country.shuffle!)
     elsif city[0][:value] == country[-2][:value]
       city[-2], city[0] = city[0], city[-2]