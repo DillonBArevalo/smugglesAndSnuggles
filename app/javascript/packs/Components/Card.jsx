1cb67fbcfe319e6c8057dd46bbec6d6a861aa438
Dillon Arevalo <dillonbarevalo@gmail.com>
2019-03-10 00:17:29 -0800

    Play Redesign: Center cards with offsets for stacks
    - Create and use formula to offset by an amount that will center cards but will allow visibility of only the card number for the card underneath
    - Pass additional classes into card so that it can render what position in the stack it is
    - Use SASS mixins to generate repetitive classes

--- app/javascript/packs/Components/Card.jsx
+++ app/javascript/packs/Components/Card.jsx
@@ -21,8 +21,9 @@ class Card extends Component {
     return(
       <img
         src={this.props.url}
-        className={`card stack-${this.props.zIndex} card--${this.props.deck} ${this.props.class || ''}`}
+        className={`card ${this.props.stackClass} card--${this.props.deck} ${this.props.selected ? 'card--selected' : ''} ${this.props.class || ''}`}
         onClick={this.clickHandler}
+        alt={this.props.faceDown ? `Facedown ${this.props.deck} card` : `${this.props.deck} ${this.props.value} card`}
       />
     );
   }
--- app/javascript/packs/Components/Cell.jsx
+++ app/javascript/packs/Components/Cell.jsx
@@ -20,26 +20,31 @@ class Cell extends Component {
   }
 
   render() {
+    const numCards = this.props.cards.length;
+    const isOverflowing = numCards > 3;
     return(
       <div
         className={`board__cell ${(this.props.highlighted && 'board__cell--highlighted') || ''}`}
         onClick={this.props.highlighted ? this.highlightClick : this.props.showStack}
       >
         {this.props.cards.map((card, idx) => {
-          return  <Card
-                    key={`${card.deck}${card.value}`}
-                    deck={card.deck}
-                    value={card.value}
-                    zIndex={idx}
-                    url={this.props.getCardUrl(card.deck, card.value, card.faceDown)}
-                    faceDown={card.faceDown}
-                    highlightMoves={this.props.highlightMoves}
-                    cancelMove={this.props.cancelMove}
-                    active={card.active}
-                    isSmuggled={card.isSmuggled}
-                    class={(this.props.selected && idx === this.props.cards.length - 1 && 'card--selected') || ''}
-                  />
+          const position = numCards - idx;
+          const stackSize = numCards - 3;
+          return idx >= stackSize && <div className="card-container"><Card
+              key={`${card.deck}${card.value}`}
+              deck={card.deck}
+              value={card.value}
+              url={this.props.getCardUrl(card.deck, card.value, card.faceDown)}
+              faceDown={card.faceDown}
+              highlightMoves={this.props.highlightMoves}
+              cancelMove={this.props.cancelMove}
+              active={card.active}
+              isSmuggled={card.isSmuggled}
+              selected={this.props.selected && position === 1}
+              stackClass={`card--${position}-of-${isOverflowing ? 4 : numCards}`}
+            /></div>
         })}
+        {/* {isOverflowing && <div className="card card--4-of-4"> + {numCards - 3}</div>} */}
       </div>
     );
   }
