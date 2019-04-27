37ccad333795a29c0902d9d93e08c1e72337f042
Dillon Arevalo <dillonbarevalo@gmail.com>
2019-04-27 15:14:30 -0700

    Style profile past games table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :users_won_games
   has_many :won_games, through: :users_won_games, source: :game
   has_many :games_users
-  has_many :games, through: :games_users
+  has_many :games, -> { order "updated_at DESC" }, through: :games_users
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "no-photo.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
