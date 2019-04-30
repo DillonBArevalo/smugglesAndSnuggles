580ba64122faec846f393c10267a450da1ab74fd
Dillon Arevalo <dillonbarevalo@gmail.com>
2019-04-29 19:59:46 -0700

    Add icons to profile table

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,4 +10,14 @@ module UsersHelper
     def is_winner(game)
         game.winner == current_user
     end
+
+    def generateIcon(game)
+        if game.completed_at
+            image = image_tag('inspectIcon.png', alt: 'Revisit game', class: 'game-history-table__icon')
+            link_to(image, "/games/#{game.id}", class: 'game-history-table__icon-link')
+        else
+            image = image_tag('redoIcon.png', alt: 'Resume game', class: 'game-history-table__icon')
+            link_to(image, "/games/#{game.id}/play", class: 'game-history-table__icon-link')
+        end
+    end
 end
