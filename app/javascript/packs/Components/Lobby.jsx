35a69d896c04270d67eb93feb1214f8e1fe27ba6
Dillon Arevalo <dillonbarevalo@gmail.com>
2019-08-29 21:55:27 -0700

    Game Lobby: Use action cable instead of pubnub for lobby
    - Remove unneeded methods from the lobby component
    - Add functionality for broadcasting joining the lobby
    - Render a local game button on the home page for testing purposes
    - Add streaming options with action cable to rails for the lobby

--- app/javascript/packs/Components/Lobby.jsx
+++ app/javascript/packs/Components/Lobby.jsx
@@ -2,153 +2,78 @@ import React, { Component } from 'react';
 import Player from "./Player";
 import ChallengeTile from "./ChallengeTile";
 import ChallengeRequests from "./ChallengeRequests";
-import {fetchKeysAndEnterLobby, challengePlayerById, acceptChallenge, newGame} from "../modules/apiRequests";
+import { enterLobby } from "../modules/apiRequests";
 
 class Lobby extends Component {
   constructor(props){
     super(props);
     this.state = {
       players: {},
-      selectedPlayer: null,
-      id: Number(this.props.id),
       challengeMessage: '',
       requests: [],
-      name: this.props.name,
     };
-    this.acceptChallenge = acceptChallenge.bind(this);
-    this.confirmInvitation = this.confirmInvitation.bind(this);
+
+    this.messageResponseMapper = {
+      join: 'addNewPlayer',
+      leave: 'removePlayer',
+    }
     this.handleMessage = this.handleMessage.bind(this);
-    this.managePlayerPresence = this.managePlayerPresence.bind(this);
-    this.pingPlayerById = this.pingPlayerById.bind(this);
-    this.setPlayerInviteStatus = this.setPlayerInviteStatus.bind(this);
-    this.challengePlayerById = challengePlayerById.bind(this, this.setPlayerInviteStatus);
-    this.updateChallengeMessage = this.updateChallengeMessage.bind(this);
   }
 
   componentDidMount(){
-    fetchKeysAndEnterLobby(this);
-  }
-
-  confirmInvitation(data){
-    this.acceptChallenge(data.id);
-    // change message to accepted and disable button
-  }
-
-  setPlayerInviteStatus (status, response) {
-    if (!status.error) {
-      const selectedPlayer = Object.assign({}, this.state.selectedPlayer, {requestSent: true});
-      this.setState({selectedPlayer});
-    }
-  }
-
-  handleMessage (message) {
-    if (message.message.foe === this.state.id) {
-      if (message.message.type === 'challenge') {
-        const requests = this.state.requests;
-        requests.push({
-          id: message.message.challengerId,
-          name: message.message.challengerName,
-          message: message.message.message,
-        });
-        this.setState({requests});
-      } else if (message.message.type === 'accept') {
-        newGame(message.message.challengerId, this);
-      } else if (message.message.type === 'game start') {
-        window.location.href = message.message.href;
-      }
-    }
-  }
-
-  updateChallengeMessage (event) {
-    this.setState({challengeMessage: event.target.value});
-  }
-
-  // this probably wants to move to apiRequests?
-  managePlayerPresence (presenceEvent) {
-    const players = this.state.players;
-    let selectedPlayer = this.state.selectedPlayer;
-    if (presenceEvent.action === 'state-change') {
-      if (presenceEvent.state && this.state.id !== presenceEvent.state.id) {
-        players[presenceEvent.state.id] = presenceEvent.state;
-      }
-    } else if (presenceEvent.action === 'leave' && presenceEvent.state) {
-      delete players[presenceEvent.state.id];
-      if (selectedPlayer && presenceEvent.state.id === selectedPlayer.id) {
-        selectedPlayer = null;
-      }
-    }
-    this.setState({players, selectedPlayer});
+    enterLobby(this, this.handleMessage, {
+      id: this.props.id,
+      name: this.props.name,
+    });
   }
 
   componentWillUnmount(){
-    this.pubnub.unsubscribeAll();
+    this.channel.leave();
   }
 
-  pingPlayerById(){
-    // ping another player by id
-    const id = this.state.selectedPlayer.id;
-    console.log(id);
+  removePlayer (playerDetails) {
+    const players = Object.assign({}, this.state.players);
+    players[playerDetails.id] && delete players[playerDetails.id];
+
+    this.setState({players});
   }
 
-  newRequest(){
-    // render new request
-  }
+  addNewPlayer (playerDetails, targeted) {
+    const players = Object.assign(
+      {},
+      this.state.players,
+      {
+        [ playerDetails.id ]: playerDetails
+      }
+    )
+    this.setState({players});
 
-  placeholder(selectedPlayer){
-    console.log(selectedPlayer)
-    this.setState({selectedPlayer, challengeMessage: ''});
-  }
-
-  test() {
-    this.setState({
-      players: {
-        '1': {
-          name: 'name 1',
-          id: '1'
-        },
-        '2': {
-          name: 'name 2',
-          id: '2'
-        },
-        '3': {
-          name: 'name 3',
-          id: '3'
-        },
-        '4': {
-          name: 'name 4',
-          id: '4'
-        },
-        '5': {
-          name: 'name 5',
-          id: '5'
-        },
-        '6': {
-          name: 'name 6',
-          id: '6'
-        },
-        '7': {
-          name: 'name 7',
-          id: '7'
-        },
+    !targeted && this.channel.send({
+      type: 'join',
+      recipient: playerDetails.id,
+      playerDetails: {
+        id: this.props.id,
+        name: this.props.name,
       },
-      requests: [
-        {
-          id: '1',
-          name: 'name 1',
-          message: 'not used anymore'
-        },
-        {
-          id: '2',
-          name: 'name 2',
-          message: 'not used anymore'
-        },
-        {
-          id: '3',
-          name: 'name 3',
-          message: 'not used anymore'
-        },
-      ]
-    });
+    })
+  }
+
+  handleMessage (message) {
+    if (message.playerDetails.id !== this.props.id && (!message.recipient || message.recipient === this.props.id)) {
+      this[this.messageResponseMapper[message.type]](message.playerDetails, !!message.recipient);
+    }
+  }
+
+  renderPlayer (playerData, type, callbacks) {
+    return (
+      <Player
+        name={playerData.name}
+        id={playerData.id}
+        key={`${type}-${playerData.id}`}
+        type={type}
+        {...callbacks}
+      />
+    );
   }
 
   render() {
@@ -160,53 +85,27 @@ class Lobby extends Component {
             <h3 className="lobby__grouping-heading">Pending Invites <hr className="lobby__heading-rule" aria-hidden="true"/></h3>
             <ul className="lobby__player-list">
               {this.state.requests.map(
-                request => {
-                  const playerData = this.state.players[request.id];
-                  return <Player
-                    name={playerData.name}
-                    id={playerData.id}
-                    key={playerData.id}
-                    type="request"
-                  />
-                })}
+                request => {return this.renderPlayer(this.state.players[request.id], 'request')}
+              )}
             </ul>
           </div>
           <div className="lobby__players-online-container">
             <h3 className="lobby__grouping-heading">Players Online <hr className="lobby__heading-rule" aria-hidden="true"/></h3>
             <ul className="lobby__player-list">
               {Object.keys(this.state.players).map(
-                playerId => <Player
-                              name={this.state.players[playerId].name}
-                              id={playerId}
-                              key={playerId}
-                              type={this.state.players.type || 'default'}
-                              challenge={this.placeholder.bind(this, this.state.players[playerId])}
-                            />)}
+                playerId => {
+                  const player = this.state.players[playerId];
+                  return !player.isRequested && this.renderPlayer(
+                    player,
+                    player.type || 'default',
+                    // {challenge: this.placeholder.bind(this, this.state.players[playerId])}
+                  );
+                }
+              )}
             </ul>
           </div>
-          <button className="sessions__button" onClick={this.test.bind(this)}>test</button>
         </div>
-        {false ?
-        (<div>
-
-          <div className="lobby__challenge-tile">
-            <ChallengeTile
-              player={this.state.selectedPlayer}
-              challenge={this.challengePlayerById}
-              challengeMessage={this.state.challengeMessage}
-              updateChallengeMessage={this.updateChallengeMessage}
-              />
-          </div>
-          <ChallengeRequests
-            requests={this.state.requests}
-            confirmInvitation={this.confirmInvitation}
-            />
-
-        </div>)
-            : ''
-          }
       </div>
-
     );
   }
 }
