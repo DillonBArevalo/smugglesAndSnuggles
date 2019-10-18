82a77a00005a8e46e417c96dae562687db1bdb65
Dillon Arevalo <dillonbarevalo@gmail.com>
2019-10-17 19:47:59 -0700

    Add modal focus trapping

--- app/javascript/packs/Components/Modal.jsx
+++ app/javascript/packs/Components/Modal.jsx
@@ -5,24 +5,49 @@ class Modal extends Component {
   constructor(props) {
     super(props);
     this.closeOnEsc = this.closeOnEsc.bind(this);
+    this.trapTab = this.trapTab.bind(this);
   }
 
   componentDidMount () {
     this.root = document.getElementById('modal');
     this.hideOtherContent();
-    this.trapTab();
     this.trapScroll();
-    document.getElementById('modalHeading').focus();
+    console.log('element', this.props.firstFocusId, document.getElementById(this.props.firstFocusId || 'modalHeading'))
+    document.getElementById(this.props.firstFocusId || 'modalHeading').focus();
   }
+
   componentWillUnmount () {
     this.unhideOtherNodes();
     document.getElementById(this.props.returnFocusTo).focus();
   }
-  trapTab () {
+
+  trapTab (event) {
+    if (event.keyCode !== 9) {
+      return;
+    } else {
+      event.preventDefault();
+    }
+
+    if (this.props.tabbableElementIds){
+      this.focusNextElement(event);
+    } else {
+      document.getElementById('modalCloseButton').focus();
+    }
   }
+
+  focusNextElement(event) {
+    const {tabbableElementIds} = this.props;
+    const listLength = tabbableElementIds.length;
+    const currentIndex = tabbableElementIds.indexOf(document.activeElement.id) || 0;
+    const direction = event.shiftKey ? -1 : 1;
+    const nextIndex = (currentIndex + direction + listLength) % listLength;
+    document.getElementById(tabbableElementIds[nextIndex]).focus();
+  }
+
   trapScroll () {
 
   }
+
   hideOtherContent () {
     const children = document.body.children;
     const nodeStates = [];
@@ -35,6 +60,7 @@ class Modal extends Component {
     }
     this.unhideOtherNodes = this.unhideOtherNodes.bind(this, nodeStates);
   }
+
   unhideOtherNodes (nodeStates) {
     nodeStates.forEach(nodeData => {
       nodeData.node.setAttribute('aria-hidden', nodeData.ariaHiddenState);
@@ -42,12 +68,16 @@ class Modal extends Component {
   }
 
   closeOnEsc (event) {
-    console.log(event);
-    event.keycode === 27 && this.props.closeModal
+    event.keyCode === 27 && this.props.closeModal();
   }
 
   renderModalContent () {
-    return (<div id="modal" className="modal" onKeyUp={this.closeOnEsc}>
+    return (<div
+      id="modal"
+      className="modal"
+      onKeyUp={this.closeOnEsc}
+      onKeyDown={this.trapTab}
+    >
       <div className="modal__backdrop" onClick={this.props.closeModal}></div>
       <div className="modal__main" role="dialog">
         {this.props.children ? this.props.children : <Fragment>
@@ -56,13 +86,14 @@ class Modal extends Component {
             className="modal__title"
             tabIndex="-1"
             >
-            Feature coming soon\u2122
+            Feature coming soon\u2122!
           </h1>
           <button
+            id="modalCloseButton"
             className="modal__button"
             onClick={this.props.closeModal}
-            >
-            Okay
+          >
+            Close
           </button>
         </Fragment>}
       </div>
