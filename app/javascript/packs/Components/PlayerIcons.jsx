a473250bebbd283b23d5b62c65dc1ff4e8a9474c
Dillon Arevalo <dillonbarevalo@gmail.com>
2020-01-20 19:36:26 -0800

    Add first pass at using JS to set profile picture size on game page
    - Needs responsivity - doesn't react to resize.
    - Text isn't offset correctly (though might keep it that way)
    - won't work at very wide screens (doesn't account for flex-grow)
    - Modifies moves counter: Currently centers it but upon making a move it all shifts. probably want to find a better solution
    - Modifies style on active turn: now 60/40 instead of 55/45

--- app/javascript/packs/Components/PlayerIcons.jsx
+++ app/javascript/packs/Components/PlayerIcons.jsx
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
 
+const MOVE_CONTAINER_HEIGHT = 3;
+const TURN_MARGIN_HEIGHT = 1;
+const PICTURE_CONTAINER_MARGIN_HEIGHT = 3;
+const STATIC_ITEMS_HEIGHT = MOVE_CONTAINER_HEIGHT + TURN_MARGIN_HEIGHT + PICTURE_CONTAINER_MARGIN_HEIGHT;
+const WRAPPING_WIDTHS_IN_PX = {
+  country: [1337, 787],
+  city: [1076, 749]
+};
+const LINE_HEIGHT = 2.5;
+
+const getActivePlayerTextHeight = (screenWidth, deck) => {
+  const numberOfLines = WRAPPING_WIDTHS_IN_PX[deck].reduce(
+    (wraps, size) => size < screenWidth ? wraps + 1 : wraps,
+    1
+  );
+  return numberOfLines * LINE_HEIGHT;
+};
+
+const getImageHeight = (deck, isActive) => {
+  const userPane = ((isActive ? 0.6 : 0.4) * document.body.offsetHeight) / 16;
+  const playerTextHeight = isActive ? getActivePlayerTextHeight(document.body.offsetWidth, deck) : 0;
+  return userPane - STATIC_ITEMS_HEIGHT - playerTextHeight;
+};
+
+const getImageSize = (containerWidth, deck, isActive) => {
+  const usableWidth = (containerWidth / 16) - 1;
+  return Math.min(usableWidth, getImageHeight(deck, isActive));
+}
+
+const getAllImageSizes = containerWidth => ({
+  city: {
+    active: getImageSize(containerWidth, 'city', true),
+    notActive: getImageSize(containerWidth, 'city', false),
+  },
+  country: {
+    active: getImageSize(containerWidth, 'country', true),
+    notActive: getImageSize(containerWidth, 'country', false),
+  }
+});
+
 class PlayerIcons extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      mapping: {
-        country: this.props.flipped ? 'top' : 'bottom',
-        city: this.props.flipped ? 'bottom' : 'top',
-        top: this.props.flipped ? 'country' : 'city',
-        bottom: this.props.flipped ? 'city' : 'country',
-      }
+    this.mapping = {
+      country: this.props.flipped ? 'top' : 'bottom',
+      city: this.props.flipped ? 'bottom' : 'top',
+      top: this.props.flipped ? 'country' : 'city',
+      bottom: this.props.flipped ? 'city' : 'country',
     }
+    const defaultContainerWidth = document.body.offsetWidth * 0.3;
+    this.state = {
+      imageSizes: getAllImageSizes(defaultContainerWidth)
+    };
   }
 
   getPlayerData(position, key) {
-    return this.props.playersData[this.state.mapping[position]][key]
+    return this.props.playersData[this.mapping[position]][key]
   }
 
-  generateUserPicture(position){
-    const deck = this.state.mapping[position];
+  generateUserPicture(position, isActive){
+    const deck = this.mapping[position];
+    const imageSize = `${this.state.imageSizes[deck][isActive ? 'active' : 'notActive']}rem`;
+    const imageStyle = {height: imageSize, width: imageSize};
     return (
       <div className="player-icons__user-picture-container">
         {this.props.winner && this.props.winner === deck &&
@@ -29,9 +73,10 @@ class PlayerIcons extends Component {
           />
         }
         <img
-          alt={`user ${this.getPlayerData(position, 'username')}'s' profile picture`}
+          alt={`${this.getPlayerData(position, 'username')}'s profile picture`}
           src={this.getPlayerData(position, 'url')}
           className="player-icons__profile-picture"
+          style={imageStyle}
         />
         <p className={`player-icons__user-name player-icons__user-name--${deck}`}>
           {this.getPlayerData(position, 'username')}
@@ -64,7 +109,7 @@ class PlayerIcons extends Component {
       <p
         {...attrs}
       >
-        {`${this.state.mapping[position]} Bears' Turn`}
+        {`${this.mapping[position]} Bears' Turn`}
       </p>
     );
   }
@@ -80,13 +125,13 @@ class PlayerIcons extends Component {
         'player-icons__player-container--bottom',
       ],
     };
-    const activePosition = this.state.mapping[this.props.active];
-    classes[this.state.mapping.country].push('player-icons__player-container--country');
-    classes[this.state.mapping.city].push('player-icons__player-container--city');
+    const activePosition = this.mapping[this.props.active];
+    classes[this.mapping.country].push('player-icons__player-container--country');
+    classes[this.mapping.city].push('player-icons__player-container--city');
     classes[activePosition].push('player-icons__player-container--active');
 
     return (
-      <div className="player-icons">
+      <section className="player-icons" aria-label="Player information">
         <div className={classes.top.join(' ')}>
           <div className='player-icons__move-container'>
             {activePosition === 'top' &&
@@ -95,12 +140,12 @@ class PlayerIcons extends Component {
               </p>
             }
           </div>
-          {this.generateUserPicture('top')}
+          {this.generateUserPicture('top', activePosition === 'top')}
           {this.generateTurnData(activePosition, 'top')}
         </div>
         <div className={classes.bottom.join(' ')}>
           {this.generateTurnData(activePosition, 'bottom')}
-          {this.generateUserPicture('bottom')}
+          {this.generateUserPicture('bottom', activePosition !== 'top')}
           <div className='player-icons__move-container'>
             {activePosition === 'bottom' &&
               <p className="player-icons__move-data" aria-label={`Moves left: ${this.props.movesLeft}`}>
@@ -109,7 +154,7 @@ class PlayerIcons extends Component {
             }
           </div>
         </div>
-      </div>
+      </section>
     );
   };
 }
